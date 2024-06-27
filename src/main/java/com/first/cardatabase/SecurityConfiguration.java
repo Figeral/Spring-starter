3fd23016bfebc811e17d1da6b210ca6762fd770a
Figeral <mouliofitzgerard@gmail.com>
2024-06-27 10:32:29 +0100

    configuration on security done

--- src/main/java/com/first/cardatabase/SecurityConfiguration.java
+++ src/main/java/com/first/cardatabase/SecurityConfiguration.java
@@ -1,26 +1,31 @@
-package com.first.cardatabase;
+// package com.first.cardatabase;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.context.annotation.Configuration;
+// import
+// org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+// import
+// org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import
+// org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import com.first.cardatabase.service.*;;
+// import com.first.cardatabase.service.*;;
 
-@Configuration
-@EnableWebSecurity
-public class SecurityConfiguration {
-    @Autowired
-    UserDetailsServicesImpl UserDetailsServicesImpl;
+// @Configuration
+// @EnableWebSecurity
+// public class SecurityConfiguration {
+// @Autowired
+// UserDetailsServicesImpl UserDetailsServicesImpl;
 
-    @Autowired
-    public void GlobalConfig(AuthenticationManagerBuilder auth) throws Exception {
-        try {
-            auth.userDetailsService(UserDetailsServicesImpl).passwordEncoder(new BCryptPasswordEncoder());
-        } catch (Exception e) {
-            System.err.println((e.getMessage()));
-        }
-    }
-}
+// @Autowired
+// public void GlobalConfig(AuthenticationManagerBuilder auth) throws Exception
+// {
+// try {
+// auth.userDetailsService(UserDetailsServicesImpl).passwordEncoder(new
+// BCryptPasswordEncoder());
+// } catch (Exception e) {
+// System.err.println((e.getMessage()));
+// }
+// }
+// }
--- src/main/java/com/first/cardatabase/service/UserDetailsServicesImpl.java
+++ src/main/java/com/first/cardatabase/service/UserDetailsServicesImpl.java
@@ -1,36 +1,36 @@
-package com.first.cardatabase.service;
+// package com.first.cardatabase.service;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
+// import java.util.Collection;
+// import java.util.List;
+// import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.security.core.userdetails.UserDetails;
+// import org.springframework.security.core.userdetails.UserDetailsService;
+// import org.springframework.security.core.userdetails.UsernameNotFoundException;
+// import org.springframework.stereotype.Service;
 
-import com.first.cardatabase.domain.User;
-import com.first.cardatabase.domain.UserRepository;
+// import com.first.cardatabase.domain.User;
+// import com.first.cardatabase.domain.UserRepository;
 
-@Service
-public class UserDetailsServicesImpl implements UserDetailsService {
-    @Autowired
-    UserRepository userRepo;
+// @Service
+// public class UserDetailsServicesImpl implements UserDetailsService {
+//     @Autowired
+//     UserRepository userRepo;
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<User> user = userRepo.findByUsername(username);
-        if (user.isPresent()) {
-            var instanceUser = user.get();
-            var builder = org.springframework.security.core.userdetails.User.withUsername(username);
-            builder.password(instanceUser.getPassword());
-            builder.roles(instanceUser.getRole());
-            return builder.build();
-        } else {
-            throw new UsernameNotFoundException("User name not found ");
-        }
+//     @Override
+//     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//         Optional<User> user = userRepo.findByUsername(username);
+//         if (user.isPresent()) {
+//             var instanceUser = user.get();
+//             var builder = org.springframework.security.core.userdetails.User.withUsername(username);
+//             builder.password(instanceUser.getPassword());
+//             builder.roles(instanceUser.getRole());
+//             return builder.build();
+//         } else {
+//             throw new UsernameNotFoundException("User name not found ");
+//         }
 
-    }
+//     }
 
-}
+// }
